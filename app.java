645bdb9e517669e8d0f4144cc3185b98719a8fb5
Jerry Sekuna <ethel203@gmail.com>
2022-02-18 07:49:51 -0500

    Java script

---
+++ app.java
@@ -0,0 +1,11 @@
+<< Git: Versioning Software (95% plus companies) 
+Distributed Version Control system. DVCS
+Open source
+Support Data integrity
+support ROLL-BACK 
+Fast 
+Collaboration is great 
+SVN --> Sub Version
+CVS --> Concurrent Version system
+TFS --> Team Foundation Server
+<<

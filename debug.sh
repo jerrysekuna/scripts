e7475c5dfbb907e29c079f5338bf7f7456e83de0
Jerry Sekuna <ethel203@gmail.com>
2022-02-18 07:59:50 -0500

    shell scripts

---
+++ debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Welcome to this beautiful morning"
+date
+pwd
+echo "You have login as" `whoami`
---
+++ math.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sum=`expr $1 + $2 + $3`
+echo "The total is" $sum
+echo "The sum is" `date`
+
